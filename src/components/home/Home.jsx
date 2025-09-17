3631bc85cf8db2bf69ddddcfa776eaa0eaf77623
HarshanaKDGS <sachinharshana01@gmail.com>
2025-09-17 21:14:27 +0530

    Node modules

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,7 @@
-// Write all the code here
-
-
-
+import React from 'react';
+import '../styles/home.scss';
+import Footer from './layout/Footer.jsx';
+import MenuCard from './MenuCard.jsx';
+import founder from './Founder.jsx';
+import burger1 from '../assets/burger1.png';
+import bg from '../assets/bg.png';
--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,1 +1,23 @@
-// Write all the code here
+import React from 'react';
+import { FaYoutube, FaInstagram } from "react-icons/fa";
+
+
+const Footer = () => {
+  return (
+    <footer>
+      {Hi}
+
+      <aside>
+        <h4>Social Media</h4>
+        <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
+          <FaYoutube />
+        </a>
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </a>
+      </aside>
+    </footer>
+  );
+};
+
+export default Footer;
